ca4b91bed712cec00678d942cb7ebce600722ec3
mustafa2008almarji <163627409+mustafa2008almarji@users.noreply.github.com>
2024-03-16 10:17:34 +0400

    Add files via upload

---
+++ project_calculator.py
@@ -0,0 +1,15 @@
+import streamlit as st
+def calculate_emi(p,n,r):
+	emi= p*(r/100)*((1+(r/100)**n)/(((1+(r/100))**n)-1)
+  st.write("emi is",emi)
+st.title("emi calculator")
+p=st.slider("principal",1000,1000000)
+tenure=st.slider("loan period",1,30)
+roi=st.slider("Rate of interset in %",1,15)
+r=roi/12
+n=tenure*12
+if st.button("calculate emi"):
+  calculate_emi(p,n,r)
+  
+
+
